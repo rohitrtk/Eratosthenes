ee6044502619a600bad304f434ecca777c74a0bf
Rohit <rohit.kisto@mail.utoronto.ca>
2019-10-27 01:28:03 -0400

    Started writing staging function

--- sieve.c
+++ sieve.c
@@ -4,9 +4,53 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <math.h>
 
 #define PIPE_READ 0
 #define PIPE_WRITE 1
+
+#define STAGE_ERROR -1
+
+/*
+ * Reads an integer from r and writes it to w if
+ * it is not a multiple of m. Returns 1 on error.
+ */
+int filter(int m, int r, int w)
+{
+    int buffer;
+    if(read(r, &buffer, sizeof(int)) == -1)
+    {
+        perror("Filter");
+        return EXIT_FAILURE;    
+    }
+
+    if(buffer % m != 0)
+    {
+        if(write(w, &buffer, sizeof(int)) == -1)
+        {
+            return EXIT_FAILURE;
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
+
+/*
+ * 
+ * 
+ * 
+ * 
+ */
+pid_t makeStage(int m, int r, int** fds)
+{
+    int fd[2];
+    if(pipe(fd) == -1)
+    {
+        perror("Stage Pipe");
+        return STAGE_ERROR;
+    }
+
+}
 
 int main(int argc, char** argv)
 {
@@ -23,7 +67,7 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-    int pipe;
+    int p;
 
     pid_t f = fork();
 
@@ -37,16 +81,37 @@ int main(int argc, char** argv)
         // Reads data from pipe
         close(fd[PIPE_WRITE]);
 
-        pipe = fd[0];
+        p = fd[PIPE_READ];
 
-        int buffer[10];
-        
-        for(int i = 0; i < 10; ++i)
+        int* dataPipe = malloc(sizeof(int*) * 2);
+        if(pipe(dataPipe) == -1)
         {
-            read(pipe, buffer, 1);
-            write(STDOUT_FILENO, buffer, 1);
-            write(STDOUT_FILENO, "\n", 1);
+            perror("Data Pipe");
+            exit(EXIT_FAILURE);
         }
+
+        int filter;
+        read(p, &filter, sizeof(int));
+        makeStage(filter, p, &dataPipe);
+
+        free(dataPipe);
+
+        /*
+        int buffer;
+        int bytesRead;
+        while((bytesRead = read(p, &buffer, sizeof(int))) > 0)
+        {
+            int length = snprintf(NULL, 0, "%d", buffer);
+            char* str = malloc(length + 1);
+
+            snprintf(str, length + 1, "%d", buffer);
+            
+            write(STDOUT_FILENO, str, sizeof(str));
+            write(STDOUT_FILENO, "\n", 1);
+
+            free(str);
+        }
+        */
 
         close(fd[PIPE_READ]);
 
@@ -57,12 +122,16 @@ int main(int argc, char** argv)
         // Writes data to pipe
         close(fd[PIPE_READ]);
 
-        pipe = fd[1];
+        p = fd[1];
 
-        for(int i = 0; i < 10; ++i)
+        int  n          = strtol(argv[1], NULL, 10);
+        int  rn         = (int)sqrt(n);
+        int* numbers    = malloc((n - 1)* sizeof(int)); // Array of numbers from 2 to n
+        
+        for(int i = 2; i < rn + 1; ++i)
         {
-            char c = 48 + i;
-            write(pipe, &c, sizeof(char));
+            numbers[i - 2] = i;
+            write(p, &numbers[i - 2], sizeof(int));
         }
 
         close(fd[PIPE_WRITE]);
@@ -72,6 +141,7 @@ int main(int argc, char** argv)
 
         if(WIFEXITED(status) && WIFEXITED(status) == 255)
         {
+            free(numbers);
             exit(255);
         }
     }
